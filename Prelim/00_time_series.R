9b056657006208d4ce3dd7e1af3bb607b6d58698
georgiab123 <georgiabradley@college.harvard.edu>
2022-10-26 11:57:04 -0400

    00_time_series.R

--- Prelim/00_time_series.R
+++ Prelim/00_time_series.R
@@ -3,6 +3,8 @@
 library(dplyr)
 library(lubridate)
 library(ggplot2)
+library(tseries)
+
 
 # Importing data ##############################################################
 
@@ -21,7 +23,7 @@ colnames(Dutch_TTF) <- c("Date", "Symbol", "Open", "High", "Low", "Close")
 #Dutch TTF ranges from 2017-05-22 --> 2022-09-26
 #Henry Hub ranges to 2022-09-16
 #Get subset of Henry Hub price data starting from 2017-05-22
-HenryHub.2017.05.19 <- HenryHub %>%  
+HenryHub_sub <- HenryHub %>%  
   filter(Date >= as.Date('2017-05-19') & Date <= as.Date('2022-09-16'))
 
 
@@ -34,8 +36,8 @@ plot(Dutch_TTF$Date,
      xlab = "Year",
      ylab = "Values",
     col = 2)
-lines(HenryHub.2017.05.19$Date,                            
-      HenryHub.2017.05.19$Price,
+lines(HenryHub_sub$Date,                            
+      HenryHub_sub$Price,
       type = "l",
       col = 3)
 legend("topleft",                           
@@ -47,10 +49,21 @@ legend("topleft",
 
 ggplot() +
   geom_line(data = Dutch_TTF, aes(x = Date, y = Close, color="Dutch LNG futures"), color = "red")  +
-  geom_line(data = HenryHub.2017.05.19, aes(x = Date, y = Price, color="Henry Hub spot price"), color = "blue") +
+  geom_line(data = HenryHub_sub, aes(x = Date, y = Price, color="Henry Hub spot price"), color = "blue") +
   labs(colour="Legend",x="Date",y="Dollars per one Million British Thermal Units (MMBTU)") +
-  geom_vline(xintercept=as.numeric(HenryHub.2017.05.19$Date[198]), linetype=3, color = "black")
+  geom_vline(xintercept=as.numeric(HenryHub_sub$Date[198]), linetype=3, color = "black")
 
 
-ccf(Dutch_TTF$Close,HenryHub.2017.05.19$Price)
-print(ccf(Dutch_TTF$Close,HenryHub.2017.05.19$Price))
+
+# data analysis ###############################################################
+ccf(Dutch_TTF$Close,HenryHub_sub$Price)
+print(ccf(Dutch_TTF$Close,HenryHub_sub$Price))
+
+
+
+# checking if time series is stationary using Dickey-Fuller Test:
+# our time series are non-stationary
+
+adf.test(HenryHub$Price)
+adf.test(HenryHub_sub$Price)
+
