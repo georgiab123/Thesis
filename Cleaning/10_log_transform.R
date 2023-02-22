757974ba4f1f001fcd668fa5f810a64ee738f5b8
georgiab123 <georgiabradley@college.harvard.edu>
2023-02-22 14:00:09 -0500

    adding the redone export/import graphs

--- Cleaning/10_log_transform.R
+++ Cleaning/10_log_transform.R
@@ -13,7 +13,7 @@ term_ts_hh
 index_max <- which.max(term_ts_hh)
 index_max
 # hh_prices_00 is the underlying time series
-# there is a spike on february the 16th at index_max of 8782 2020? 
+# there is a spike on february the 16th at index_max of 8782? 
 # thus we remove 8781, 8782, and 8783
 test <- term_ts_hh
 test[8782] <- NA
@@ -271,5 +271,64 @@ for(i in ts_names){
 
 for(i in)
 
+  
+# for plotting purposes:
+  
+ttf_plot <- ts(ttf_daily_copy$price, start = c(2010, 3), frequency = 365)
+jkm_plot <- ts(JKM_df$price, start = c(2014, 213), frequency = 365)
 
-   
+jkm_plot <- na.interp(jkm_plot, linear = TRUE)
+hh_plot <- na.interp(term_ts_hh, linear = TRUE)
+nbp_plot <- na.interp(nbp_ts_og, linear = TRUE)
+ttf_plot <- na.interp(ttf_plot, linear = TRUE)
+# going to plot all graphs
+all_ts <- cbind(hh_plot, nbp_plot, ttf_plot, jkm_plot)
+t.start <- time(hh_monthly)[220]
+window_1 <- window(all_ts, start = t.start)
+
+ts.plot(window_1, gpars = list(col = c(6,2,12,5)), xlim=c(2016,2022.6), ylab="USD/MMBtu")
+legend("topleft", legend = c("Henry Hub", "NBP", "TTF", "JKM"), col = c(6,2,12,5), lty = 1)
+abline(v = time(hh_monthly)[288], lty = 2,  col = alpha("black", 0.7))   
+abline(v = time(hh_monthly)[302], lty = 2, col = alpha("black", 0.7)) 
+
+
+
+# now redoing exports and imports:
+
+
+ng_imports_USA <- ng_imports_USA[-c(1:2),]
+ng_imports_USA <- ng_imports_USA[-c(1:288),]
+colnames(ng_imports_USA) <- c("date", "price")
+ts_ng_imports <- ts(ng_imports_USA$price, start = c(1997,1), frequency = 12 )
+
+ng_exports_USA <- ng_exports_USA[-c(1:2),]
+ng_exports_USA <- ng_exports_USA[-c(1:288),]
+colnames(ng_exports_USA) <- c("date", "price")
+ts_ng_exports <- ts(ng_exports_USA$price, start = c(1997,1), frequency = 12 )
+
+
+exports_USA <- exports_USA[-c(1:2),]
+imports_USA <- imports_USA[-c(1:2),]
+colnames(imports_USA) <- c("date", "price")
+colnames(exports_USA) <- c("date", "price")
+ts_exports <- ts(exports_USA$price, start = c(1997,1), frequency = 12 )
+ts_imports <- ts(imports_USA$price, start = c(1997,1), frequency = 12 )
+
+ts_ei <- cbind(ts_ng_exports, ts_ng_imports) 
+ts_ei_window <- window(ts_ei, start = time(hh_monthly)[100])
+ts.plot(ts_ei, gpars = list(col = c(4,6)), ylab = "MMcf")
+legend("topleft", legend = c("Natural Gas exports", "Natural Gas imports"), col = c(4,6), lty = 1)
+abline(v = time(hh_monthly)[228], lty = 2, col = alpha("black", 0.7)) 
+abline(v = time(hh_monthly)[240], lty = 2, col = alpha("black", 0.7)) 
+axis(1, at = c(2016), srt=45, col = "black", las = 1, cex.axis = 0.7)
+axis(1, at = c(2017), srt=45, col = "black", las = 1, cex.axis = 0.7)
+
+
+
+
+
+
+
+
+
+
